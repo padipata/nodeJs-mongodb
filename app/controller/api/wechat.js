b1e827ec17525f822e026f9403875536a1805e3e
padipata <root>
2018-07-02 12:33:18 +0800

    添加微信授权

--- app/controller/api/wechat.js
+++ app/controller/api/wechat.js
@@ -3,27 +3,27 @@ const jwt = require('jsonwebtoken');
 
 module.exports = app => {
     class WechatController extends app.Controller {
-        * login() {
+        async login() {
             const code = this.ctx.params.code;
             const url = `https://api.weixin.qq.com/sns/jscode2session?appid=${app.config.appId}&secret=${app.config.appSecret}&js_code=${code}&grant_type=authorization_code`;
-            const result = yield this.ctx.curl(url, { dataType: 'json' });
+            const result = await this.ctx.curl(url, {dataType: 'json'});
             const openId = result.data.openid;
             if (openId) {
-                const hasRegister = yield this.ctx.service.user.hasRegister(openId);
+                const hasRegister = await this.ctx.service.user.hasRegister(openId);
                 if (!hasRegister) {
-                    yield this.ctx.service.user.register({ userId: openId, password: 'wechat' });
+                    await this.ctx.service.user.register({userId: openId, password: 'wechat'});
                 }
                 // 生成token
-                const token = jwt.sign({ userId: openId }, app.config.jwtSecret, { expiresIn: '7d' });
+                const token = jwt.sign({userId: openId}, app.config.jwtSecret, {expiresIn: '7d'});
                 this.ctx.body = token;
                 this.ctx.set('authorization', 'Bearer ' + token);
             } else {
                 this.ctx.body = '获取openId失败';
             }
             this.ctx.status = result.status;
-
         }
-        * getAccessToken() {
+
+        async getAccessToken() {
             const nowDateTime = new Date().getTime();
             const accessToken = this.ctx.session.accessToken;
             // 如果未过期，直接获取session中access_token
@@ -31,7 +31,7 @@ module.exports = app => {
                 this.ctx.body = accessToken.access_token;
             } else {
                 const url = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${app.config.appId}&secret=${app.config.appSecret}`;
-                const response = yield this.ctx.curl(url, { dataType: 'json' });
+                const response = await this.ctx.curl(url, {dataType: 'json'});
                 const result = response.data;
                 // 将accessToken存在session中
                 if (result.access_token) {
@@ -48,5 +48,6 @@ module.exports = app => {
 
         }
     }
+
     return WechatController;
 };
